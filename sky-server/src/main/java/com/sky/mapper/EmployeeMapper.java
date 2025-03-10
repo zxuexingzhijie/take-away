59daa7b481e08cb9eada5edca0fd74a79d41f7d5
hhh <2762006003@qq.com>
2025-03-10 21:59:36 +0800

    员工分页查询功能完善
    - 采用pagehelper插件进行分页查询
    - 动态SQL进行拼接
    - 返回的日期采用JSONFormat注解进行格式化

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,5 +1,7 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -19,4 +21,13 @@ public interface EmployeeMapper {
     @Insert("insert into sky.employee (username, name, password, phone, sex, id_number, status, create_time, update_time, create_user, update_user) " +
             "values (#{username}, #{name}, #{password}, #{phone}, #{sex}, #{idNumber}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
     void addEmploy(Employee employee);
+
+
+    /**
+     * 分页查询
+     * @param employeeQueryDTO
+     * @return
+     */
+
+    Page<Employee> pagequery(EmployeePageQueryDTO employeeQueryDTO);
 }
