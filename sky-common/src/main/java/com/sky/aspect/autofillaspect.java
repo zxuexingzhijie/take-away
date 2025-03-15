981af13a5e2fe70b4973389120f37dc3b2e4aedc
hhh <2762006003@qq.com>
2025-03-15 16:07:32 +0800

    公共字段填充

--- sky-common/src/main/java/com/sky/aspect/autofillaspect.java
+++ sky-common/src/main/java/com/sky/aspect/autofillaspect.java
@@ -1,12 +1,21 @@
 package com.sky.aspect;
 
 
+import com.sky.annotation.autofill;
+import com.sky.context.BaseContext;
+import com.sky.enumeration.OperationType;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.time.LocalDateTime;
 
 /**
  * 自定义切面类
@@ -28,7 +37,42 @@ public class autofillaspect {
      * 公共字段填充
      */
     @Before("autofillpointcut()")
-    public void autofill(JoinPoint joinPoint){
+    public void autofill(JoinPoint joinPoint) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         log.info("开始执行公共字段 填充");
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        autofill annotation = signature.getMethod().getAnnotation(autofill.class);
+        if (annotation == null){
+            return;
+        }else{
+            Object[] args = joinPoint.getArgs();
+            if (args == null || args.length == 0){
+                return;
+            } else {
+                Object arg = args[0];
+                LocalDateTime now = LocalDateTime.now();
+                Long currentId = BaseContext.getCurrentId();
+                if (annotation.value() == OperationType.INSERT){
+                    Method setCreateTime = arg.getClass().getMethod("setCreateTime", LocalDateTime.class);
+                    Method setCreateUser = arg.getClass().getMethod("setCreateUser", Long.class);
+                    Method setUpdateTime = arg.getClass().getMethod("setUpdateTime", LocalDateTime.class);
+                    //getmethod()获取的是公共方法, getDeclaredMethod()获取的是所有方法,无论是否公有或者私有
+                    Method setUpdateUser = arg.getClass().getMethod("setUpdateUser", Long.class);
+                    setCreateTime.invoke(arg,now);
+                    setCreateUser.invoke(arg,currentId);
+                    setUpdateTime.invoke(arg,now);
+                    setUpdateUser.invoke(arg,currentId);
+                }else if (annotation.value() == OperationType.UPDATE){
+                    Method setUpdateTime = arg.getClass().getMethod("setUpdateTime", LocalDateTime.class);
+                    Method setUpdateUser = arg.getClass().getMethod("setUpdateUser", Long.class);
+                    setUpdateUser.invoke(arg,currentId);
+                    setUpdateTime.invoke(arg,now);
+                }
+
+            }
+
+        }
+
+
+
     }
 }
